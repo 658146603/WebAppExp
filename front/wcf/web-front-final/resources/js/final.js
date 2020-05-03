d748594df1e74866591571ef85474761d9e28f30
wcf <1533144693@qq.com>
2020-05-03 17:30:34 +0800

    update

--- front/wcf/web-front-final/resources/js/final.js
+++ front/wcf/web-front-final/resources/js/final.js
@@ -1,6 +1,37 @@
 let drawer = new mdui.Drawer('#drawer');
 let $ = mdui.JQ;
-window.onload = function () {
+let news_tab = document.getElementById("news-tab");
+let news_list_0 = document.getElementById("news-list-0");
+let news_list_1 = document.getElementById("news-list-1");
+let info_list_0 = document.getElementById("info-list-0");
+let info_tab = document.getElementById("info-tab");
+let info_list_1 = document.getElementById("info-list-1");
+let info_list_2 = document.getElementById("info-list-2");
 
+window.onload = function () {
+    news_tab.addEventListener('change.mdui.tab', function (event) {
+        if (event._detail.index === 0) {
+            news_list_0.style.display = 'block';
+            news_list_1.style.display = 'none';
+        } else {
+            news_list_0.style.display = 'none';
+            news_list_1.style.display = 'block';
+        }
+    });
+    info_tab.addEventListener('change.mdui.tab', function (event) {
+        if (event._detail.index === 0) {
+            info_list_0.style.display = 'block';
+            info_list_1.style.display = 'none';
+            info_list_2.style.display = 'none';
+        } else if (event._detail.index === 1) {
+            info_list_0.style.display = 'none';
+            info_list_1.style.display = 'block';
+            info_list_2.style.display = 'none';
+        } else {
+            info_list_0.style.display = 'none';
+            info_list_1.style.display = 'none';
+            info_list_2.style.display = 'block';
+        }
+    });
 }
 
