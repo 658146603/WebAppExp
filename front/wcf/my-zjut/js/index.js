339b2feeed79b0287e27c6cf91eafccca289b31a
wcf <1533144693@qq.com>
2020-05-14 23:14:24 +0800

    update

--- front/wcf/my-zjut/js/index.js
+++ front/wcf/my-zjut/js/index.js
@@ -7,13 +7,18 @@ let info_list_0 = document.getElementById("info-list-0");
 let info_tab = document.getElementById("info-tab");
 let info_list_1 = document.getElementById("info-list-1");
 let info_list_2 = document.getElementById("info-list-2");
-let banner_pic_index = 0;
+let pic_banner_index = 0;
 let pic_news_index = 0;
+let pic_banner_flag = true;
+let pic_news_flag = true;
 
 const accent = '#ff4081';
 const normal = '#fefefe';
 
-window.onload = function () {
+const square_normal = 'rgba(255,255,255,0.15)';
+const square_accent = 'rgba(255,64,129,0.75)';
+
+window.onload = () => {
     news_tab.addEventListener('change.mdui.tab', function (event) {
         if (event._detail.index === 0) {
             news_list_0.style.display = 'block';
@@ -40,7 +45,7 @@ window.onload = function () {
         }
     });
 
-    let newsPics = [
+    let pic_banner_list = [
         "image/banner/20191119025726.jpg",
         "image/banner/20200206071934.jpg",
         "image/banner/20200305094918.jpg",
@@ -51,7 +56,40 @@ window.onload = function () {
         "image/banner/20200415052132.jpg"
     ];
 
-    let little_dots = [
+    let pic_news_list = [
+        {
+            picture: 'image/news/1588170411953002848.jpg',
+            title: '春意正浓，学校2020年春季学期错峰开学',
+            subtitle: '文 党委宣传部 王臻<br>图 袁九生 黄捷 沈欣媛 孙滢璐',
+            content: '花香正浓，春意正旺。4月27日，我校迎来了2020年春季学期错峰开学的第一批返校学生'
+        },
+        {
+            picture: 'image/news/1583997302671044478.jpg',
+            title: '学校与德清县在莫干山校区开展植树活动',
+            subtitle: '文  党委宣传部 文 徐小芸 <br>图 陈钊',
+            content: '春和景明，水暖风煦，生机勃发。3月12日上午，学校与德清县在莫干山校区开展植树活动。'
+        },
+        {
+            picture: 'image/news/1587627065906001946.jpg',
+            title: '我校开展学生开学返校演练',
+            subtitle: '文 学生工作部 朱皆笑<br>图 人文学院 袁九生 程宸',
+            content: '4月20日上午，学校在屏峰校区举行2020年春季学期学生开学返校演练。'
+        },
+        {
+            picture: 'image/news/1587343366734009840.jpg',
+            title: '学校返校复学思政第一课在线开讲',
+            subtitle: '文 党委宣传部 王臻<br>图 李铮铮',
+            content: '4月18日下午，学校2020春季学期战疫·返校复学思政第一课在云端开讲'
+        },
+        {
+            picture: 'image/news/1576032031984009328.jpg',
+            title: '建筑工程学院举行建院40周年院庆系列活动',
+            subtitle: '图 建筑工程学院<br>文 建筑工程学院',
+            content: '近日，建筑工程学院建院四十周年院庆系列活动在屏峰举行，校党委书记蔡袁强出席活动'
+        }
+    ];
+
+    let pic_banner_little_dots = [
         document.getElementById('little-dot-0'),
         document.getElementById('little-dot-1'),
         document.getElementById('little-dot-2'),
@@ -62,54 +100,91 @@ window.onload = function () {
         document.getElementById('little-dot-7')
     ];
 
-    little_dots.forEach(function (dot, index) {
-        dot.addEventListener('mouseover', function () {
-            banner_pic_index = index;
-            $('#news-banner').attr('src', newsPics[banner_pic_index % newsPics.length]);
+    let pic_news_little_dots = [
+        document.getElementById('little-dot-1-0'),
+        document.getElementById('little-dot-1-1'),
+        document.getElementById('little-dot-1-2'),
+        document.getElementById('little-dot-1-3'),
+        document.getElementById('little-dot-1-4'),
+    ];
 
-            little_dots.forEach(function (dot, index) {
-                if (index === banner_pic_index % newsPics.length) {
+    pic_banner_little_dots[0].style.backgroundColor = accent;
+    pic_news_little_dots[0].style.backgroundColor = square_accent;
+
+    let pic_banner_handler = () => {
+        if (!pic_banner_flag) return;
+        $('#news-banner').attr('src', pic_banner_list[(++pic_banner_index) % pic_banner_list.length]);
+        pic_banner_little_dots.forEach(function (dot, index) {
+            if (index === pic_banner_index % pic_banner_list.length) {
+                dot.style.backgroundColor = accent;
+            } else {
+                dot.style.backgroundColor = normal;
+            }
+        });
+    }
+
+    let pic_news_handler = () => {
+        if (!pic_news_flag) return;
+        $('#pic_news_picture').attr('src', pic_news_list[++pic_news_index % pic_news_list.length].picture);
+        $('#pic_news_title').html(pic_news_list[pic_news_index % pic_news_list.length].title);
+        $('#pic_news_subtitle').html(pic_news_list[pic_news_index % pic_news_list.length].subtitle);
+        $('#pic_news_content').html(pic_news_list[pic_news_index % pic_news_list.length].content);
+        pic_news_little_dots.forEach(function (dot, index) {
+            if (index === pic_news_index % pic_news_list.length) {
+                dot.style.backgroundColor = square_accent;
+            } else {
+                dot.style.backgroundColor = square_normal;
+            }
+        });
+    }
+
+    pic_banner_little_dots.forEach((dot, index) => {
+        dot.addEventListener('mouseover', () => {
+            pic_banner_flag = false;
+            console.log('mouse over');
+            pic_banner_index = index;
+            $('#news-banner').attr('src', pic_banner_list[pic_banner_index % pic_banner_list.length]);
+            pic_banner_little_dots.forEach(function (dot, index) {
+                if (index === pic_banner_index % pic_banner_list.length) {
                     dot.style.backgroundColor = accent;
                 } else {
                     dot.style.backgroundColor = normal;
                 }
             });
         });
+
+        dot.addEventListener('mouseout', () => {
+            pic_banner_flag = true;
+            console.log('mouse out');
+        });
     });
 
-    let pic_banner_timer = window.setInterval(function () {
-        $('#news-banner').attr('src', newsPics[(++banner_pic_index) % newsPics.length]);
-        little_dots.forEach(function (dot, index) {
-            if (index === banner_pic_index % newsPics.length) {
-                dot.style.backgroundColor = accent;
-            } else {
-                dot.style.backgroundColor = normal;
-            }
+    pic_news_little_dots.forEach((dot, index) => {
+        dot.addEventListener('mouseover', () => {
+            pic_news_flag = false;
+            console.log('mouse over');
+            pic_news_index = index;
+            $('#pic_news_picture').attr('src', pic_news_list[pic_news_index % pic_news_list.length].picture);
+            $('#pic_news_title').html(pic_news_list[pic_news_index % pic_news_list.length].title);
+            $('#pic_news_subtitle').html(pic_news_list[pic_news_index % pic_news_list.length].subtitle);
+            $('#pic_news_content').html(pic_news_list[pic_news_index % pic_news_list.length].content);
+            pic_news_little_dots.forEach(function (dot, index) {
+                if (index === pic_news_index % pic_news_list.length) {
+                    dot.style.backgroundColor = square_accent;
+                } else {
+                    dot.style.backgroundColor = square_normal;
+                }
+            });
         });
-    }, 3000);
+        dot.addEventListener('click', (event) => {
+            event.stopPropagation();
+        });
+        dot.addEventListener('mouseout', () => {
+            pic_news_flag = true;
+            console.log('mouse out');
+        });
+    });
 
-    let pic_news_list = [
-        {
-            picture: 'image/1588170411953002848.jpg',
-            title: '春意正浓，学校2020年春季学期错峰开学',
-            subtitle: '文 党委宣传部 王臻|图 袁九生 黄捷 沈欣媛 孙滢璐',
-            content: '花香正浓，春意正旺。4月27日，我校迎来了2020年春季学期错峰开学的第一批返校学生。学校党政领导班子成员来到现场，亲切看望了返校复学的教师、同学以及现场的工作人员和志愿者。'
-        },
-        {
-            picture: 'image/1589179113765069534.jpg',
-            title: '我校与航天科技集团五院五〇八所、桐乡市人民政府签订三方战略合作协议',
-            subtitle: '文 党委宣传部 徐小芸|图 李铮铮',
-            content: '5月9日下午，中国航天科技集团有限公司第五研究院第五〇八所、桐乡市人民政府和我校战略合作协议云签约仪式在屏峰校区举行'
-        },
-        {
-            picture: 'image/1587627065906001946.jpg',
-            title: '我校开展学生开学返校演练',
-            subtitle: '文 学生工作部 朱皆笑|图 人文学院 袁九生 程宸',
-            content: '4月20日上午，学校在屏峰校区举行2020年春季学期学生开学返校演练。校长李小年，校党委副书记陈杰到场指导演练。'
-        }
-    ];
-
-    let pic_news_banner = window.setInterval(function () {
-        $('#pic_news_title').innerText = pic_news_list[pic_news_index % pic_news_list.length].title;
-    })
+    let pic_banner_timer = window.setInterval(pic_banner_handler, 3000);
+    let pic_news_timer = window.setInterval(pic_news_handler, 3000);
 }
